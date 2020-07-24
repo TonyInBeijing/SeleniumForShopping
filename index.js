05537d913e7f1c69c82c3095483926d2fceaedfb
TonyInBeijing <1300565735@qq.com>
2020-07-24 20:36:19 +0800

    自动下单

--- index.js
+++ index.js
@@ -5,91 +5,74 @@
 // 引入 chromeDriver
 require('chromedriver');
 // 引入 selenium库
-const { Builder, By, until, Key } = require('selenium-webdriver');
+const { Builder, By, until } = require('selenium-webdriver');
 // const chrome = require('selenium-webdriver/chrome');
-const { base64ToFile, runPythonFile, delay } = require('./utils');
+const { delay } = require('./utils');
+const { elementLocated } = require('selenium-webdriver/lib/until');
+const { del } = require('selenium-webdriver/http');
 // 小米账号密码
-const USERPHONE = '';
-const USERPASSWORD = '';
-// 登录滑动图文件名称
-const BACKGROUNDFILE = 'backgroundFile';
-const SWIPERFILE = 'sliderFile';
+const USERPHONE = '18500031126';
+const USERPASSWORD = 'yuehaowei1126';
 
+// 脚本主方法
 const mainVoid = async () => {
-    // 设置chromeOption
-    // const chromeOption = new chrome.Options();
-    // chromeOption.addArguments("--user-data-dir=/Users/admin/Library/Application Support/Google/Chrome/Default");
     // 创建一个chrome浏览器的实例
     const driver = new Builder()
         .forBrowser('chrome')
-        // .setChromeOptions(chromeOption)
         .build();
     // 打开小米首页
     await driver.get('https://www.mi.com');
+    // await delay(10000);
     // 点击登录
     await driver.findElement(By.xpath('//*[@id="J_siteUserInfo"]/a[1]')).click();
     // 判断是否弹出用户协议
     const hasAgreement = await driver.findElement(By.xpath('/html/body/div[2]/div[2]/div/div/div/div[3]/button[1]'));
     if (hasAgreement) {
-        delay(2000);
+        await delay(2000);
         hasAgreement.click();
     }
-    delay(1000);
+    await delay(1000);
     // 点击账号登录
-    await driver.wait(until.elementLocated(By.xpath('//*[@id="nav-tabs"]/a[1]')), 10000).click();
+    await driver.wait(until.elementLocated(By.xpath('//*[@id="nav-tabs"]/a[1]')), 100000).click();
     // 输入账号
     for (let i in USERPHONE) {
         await driver.findElement(By.id('username')).sendKeys(USERPHONE[i]);
-        await delay(300);
     }
     await delay(500);
     // 输入密码
     for (let i in USERPASSWORD) {
         await driver.findElement(By.id('pwd')).sendKeys(USERPASSWORD[i]);
-        await delay(300);
     }
-    delay(1000);
+    await delay(1000);
     // 点击登录按钮
     await driver.findElement(By.id('login-button')).click();
-    // // 找到滑动验证背景图
-    // const backgroundUrl = await driver.wait(until.elementLocated(By.xpath('//div[@class="JDJRV-bigimg"]/img')), 10000).getAttribute('src');
-    // // 找到滑动验证滑动图
-    // const sliderUrl = await driver.wait(until.elementLocated(By.xpath('//div[@class="JDJRV-smallimg"]/img')), 10000).getAttribute('src');
-    // // 保存到本地
-    // const backgroundFilePath = await base64ToFile(BACKGROUNDFILE, backgroundUrl);
-    // const sliderFilePath = await base64ToFile(SWIPERFILE, sliderUrl);
-    // // 调用opencv-python处理图片
-    // const pythonRes = await runPythonFile(backgroundFilePath, sliderFilePath);
-    // // 获取x轴偏移量
-    // const offsetX = parseInt(Number(JSON.parse(pythonRes).offsetX) * 0.782);
-    // // 获取滑块元素
-    // const sliderBar = await driver.findElement(By.className('JDJRV-slide-inner JDJRV-slide-btn'));
-    // // 创建行为
-    // let dragActions = driver.actions();
-    // // 模拟真人滑动
-    // let moveStepArr = [5, 5];
-    // let durationArr = [6, 10];
-    // while (moveStepArr.reduce((sum, num) => { return sum + num; }) <= offsetX) {
-    //     const randomStep = parseInt(Math.random() * 10);
-    //     const randomDuration = parseInt(Math.random() * 100);
-    //     moveStepArr.push(randomStep);
-    //     durationArr.push(randomDuration);
-    // }
-    // durationArr = durationArr.sort((a, b) => { return a - b; });
-    // const moveStepSum = moveStepArr.reduce((sum, num) => { return sum + num; });
-    // // 计算需要校正的值,加到数组中最后一位
-    // const reviseNum = offsetX - moveStepSum;
-    // moveStepArr[moveStepArr.length - 1] += reviseNum;
-    // // 开始移动
-    // await delay(1000);
-    // await dragActions.move({ origin: sliderBar }).press().perform();
-    // // 开始循环移动
-    // for (let i in moveStepArr) {
-    //     await dragActions.move({ x: moveStepArr[i], y: 0, duration: durationArr[i], origin: 'pointer' }).perform();
-    //     dragActions = driver.actions();
-    // }
-    // await delay(1000);
-    // // 释放鼠标左键
-    // await dragActions.release().perform();
+    await delay(2000);
+    // 点击购物车按钮
+    // 需要把想要买的东西提前加入购物车
+    await driver.findElement(By.xpath('//*[@id="J_miniCartBtn"]')).click();
+    // 点击 去结算
+    // 这个地方 可能存在 用户行为检测
+    // 需要进行模拟人工行为
+    const submitBtn = await driver.findElement(By.xpath('//*[@id="app"]/div[2]/div/div/div[2]/div[1]/div[4]/span/a'));
+    // 创建一个动作对象
+    const submitAction = driver.actions();
+    submitAction.move({ origin: submitBtn, duration: 2000 }).press().perform();
+    await delay(500);
+    submitAction.release().perform();
+    // 默认点击第一个收货地址
+    await delay(2000);
+    const defaultAddress = await driver.findElement(By.css('.address-item:first-child'));
+    await delay(1000);
+    defaultAddress.click();
+    await delay(2000);
+    // 系统自动会填充优惠券等其他信息，暂时这里先不处理
+    // 点击 立即下单
+    // 也 可能存在 用户行为检测
+    // 继续模拟一个行为
+    const createOrderBtn = await driver.findElement(By.xpath('//*[@id="app"]/div[2]/div/div/div[2]/div/div[6]/div[2]/div/a[1]'));
+    const createOrderAction = driver.actions();
+    createOrderAction.move({ origin: createOrderBtn, duration: 2000 }).press().perform();
+    await delay(500);
+    createOrderAction.release().perform();
 };
 mainVoid();
--- utils/index.js
+++ utils/index.js
@@ -2,62 +2,7 @@
  * @file 工具方法
  * @author YHW 2020-07-22
  */
-
-// 引入fs文件模块
-const fs = require('fs');
-// 引入child_process模块
-const exec = require('child_process').exec;
-
-/**
- * 
- * @param {String} fileName 文件名
- * @param {String} base64 base64字符串
- */
-const base64ToFile = (fileName, base64) => {
-    return new Promise((resolve, reject) => {
-        const base64Str = base64.replace(/^data:image\/\w+;base64,/, "");
-        const dataBuffer = Buffer.from(base64Str, 'base64');
-        const saveFileName = `./data/${fileName}_${Date.parse(new Date())}.png`;
-        fs.writeFile(saveFileName, dataBuffer, err => {
-            if (err) {
-                resolve(false);
-            }
-            else {
-                resolve(saveFileName);
-            }
-        });
-    });
-};
-
-/**
- * 
- * @param {String}} backgroundFilePath 背景图路径
- * @param {String} sliderFilePath 滑块路径
- */
-const runPythonFile = (backgroundFilePath, sliderFilePath) => {
-    return new Promise((resolve, reject) => {
-        // 获取python文件路径
-        const pythonFilUrl = `python3 ${__dirname}/INeedNumpy.py `;
-        const paramObj = `'${JSON.stringify({ backgroundFilePath, sliderFilePath })}'`;
-        exec(pythonFilUrl + paramObj, (err, stdout, stderr) => {
-            if (err) {
-                console.log(err);
-                resolve(false);
-            }
-            else {
-                resolve(stdout);
-            }
-        });
-    });
-}
-
 // 设置延时
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-// 滑动验证码失效后
-
-
-
-module.exports.base64ToFile = base64ToFile;
-module.exports.runPythonFile = runPythonFile;
 module.exports.delay = delay;
